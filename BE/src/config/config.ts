86994cd3b0708fbcf522867f731a16b34e63b782
Anna Taranchuk <taranchuk4@yahoo.com>
2025-03-06 14:36:48 +0100

    feat: add url to config

--- BE/src/config/config.ts
+++ BE/src/config/config.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const config = {
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost:5000',
+    api_url: process.env.BASE_API_URL || '',
 };
